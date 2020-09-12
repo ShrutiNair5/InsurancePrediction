66375bc220e13da3dbcd3a738f4af9d9e15b6e99
Shruti Nair <60494396+ShrutiNair5@users.noreply.github.com>
2020-09-12 13:15:26 +0530

    Update app.py

--- Insurance/app.py
+++ Insurance/app.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 model = pickle.load(open('random_forest.pkl', 'rb'))
-@app.route('/',methods=['GET'])
+@app.route('/',methods=['GET','POST'])
 
 def Home():
     return render_template('index.html')
